88ddb934cf64c2c1322c1b5974784ef8636945c3
tauraamui <adamplewis@pm.me>
2019-04-16 16:34:02 +0100

    add autossl

--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
 
 	deployDaemonURL, err := url.Parse("http://localhost:9001")
 	if err != nil {
@@ -22,5 +25,5 @@ func main() {
 
 	e.Group("/deploy", middleware.Proxy(middleware.NewRandomBalancer(targets)))
 
-	e.Logger.Fatal(e.Start(":80"))
+	e.Logger.Fatal(e.StartAutoTLS(":443"))
 }
