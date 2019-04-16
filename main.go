2c20684b93d9557b5718b6ee97915a5dee7829ea
tauraamui <adamplewis@pm.me>
2019-04-16 17:18:29 +0100

    add configuration file and parser

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/url"
 
 	"github.com/labstack/echo"
@@ -8,22 +10,42 @@ import (
 	"golang.org/x/crypto/acme/autocert"
 )
 
+type ProxyMapping struct {
+	ReverseProxyGroup string   `json:"reverse-proxy-group"`
+	TargetUrls        []string `json:"target-urls"`
+}
+
+func loadConfig(logger echo.Logger) []*ProxyMapping {
+	file, err := ioutil.ReadFile("tlb.config")
+	if err != nil {
+		logger.Fatal("cannot read configuration file")
+	}
+
+	proxyMappings := []*ProxyMapping{}
+	json.Unmarshal(file, &proxyMappings)
+
+	return proxyMappings
+}
+
 func main() {
+
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
 
-	deployDaemonURL, err := url.Parse("http://localhost:9001")
-	if err != nil {
-		e.Logger.Fatal(err)
+	targets := []*middleware.ProxyTarget{}
+	for _, proxyMapping := range loadConfig(e.Logger) {
+		for _, targetURL := range proxyMapping.TargetUrls {
+			url, err := url.Parse(targetURL)
+			if err != nil {
+				e.Logger.Fatal(err)
+			}
+			targets = append(targets, &middleware.ProxyTarget{URL: url})
+		}
+		e.Logger.Info("Registering %s targets against group %s", targets, proxyMapping.ReverseProxyGroup)
+		e.Group(proxyMapping.ReverseProxyGroup, middleware.Proxy(middleware.NewRandomBalancer(targets)))
 	}
-
-	targets := []*middleware.ProxyTarget{
-		{URL: deployDaemonURL},
-	}
-
-	e.Group("/deploy", middleware.Proxy(middleware.NewRandomBalancer(targets)))
 
 	e.Logger.Fatal(e.StartAutoTLS(":443"))
 }
