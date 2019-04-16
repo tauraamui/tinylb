8a72baea24b31ce9ee2289740c79e1d528abd80f
Adam Lewis <adamlewis@Adams-MacBook-Pro.local>
2019-04-16 14:42:23 +0100

    initial simple/explicit implmentation

--- main.go
+++ main.go
@@ -1,3 +1,36 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	router := httprouter.New()
+	router.GET("/deploy", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		serveReverseProxy("http://localhost:9000/deploy", w, r)
+	})
+
+	http.ListenAndServe(":80", router)
+}
+
+// Serve a reverse proxy for a given url
+func serveReverseProxy(target string, w http.ResponseWriter, req *http.Request) {
+	// parse the url
+	url, _ := url.Parse(target)
+
+	// create the reverse proxy
+	proxy := httputil.NewSingleHostReverseProxy(url)
+
+	// Update the headers to allow for SSL redirection
+	req.URL.Host = url.Host
+	req.URL.Scheme = url.Scheme
+	req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
+	req.Host = url.Host
+
+	// Note that ServeHttp is non blocking and uses a go routine under the hood
+	proxy.ServeHTTP(w, req)
+}
