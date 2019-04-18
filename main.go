3a19c0be8c9f21579b3806f220b52131f77e6d12
tauraamui <adamplewis@pm.me>
2019-04-18 23:06:47 +0100

    add config file parser/reader and improve logging output

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"net/url"
+	"os"
+	"strings"
 
 	"github.com/tacusci/logging"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/labstack/gommon/log"
 	"golang.org/x/crypto/acme/autocert"
 )
 
@@ -15,17 +20,52 @@ type ProxyMapping struct {
 	TargetURL  string
 }
 
+func loadProxyMappings() ([]*ProxyMapping, error) {
+	configFile, err := os.Open("tbl.config")
+	if err != nil {
+		return nil, err
+	}
+	defer configFile.Close()
+
+	proxyMappings := []*ProxyMapping{}
+
+	scanner := bufio.NewScanner(configFile)
+	configLineCount := 0
+	for scanner.Scan() {
+		configLineCount++
+		configLine := scanner.Text()
+		values := strings.Split(configLine, " ")
+		if len(values) > 0 {
+			if strings.ToLower(values[0]) == "proxy" {
+				if len(values) == 1 {
+					return nil, fmt.Errorf("config line %d, missing request uri field for proxy mapping", configLineCount)
+				}
+				if len(values) > 2 {
+					proxyMappings = append(proxyMappings, &ProxyMapping{RequestURI: values[1], TargetURL: values[2]})
+				} else {
+					return nil, fmt.Errorf("config line %d, missing target url field for proxy mapping", configLineCount)
+				}
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return proxyMappings, nil
+}
+
 func main() {
 	e := echo.New()
+	e.Logger.SetLevel(log.DEBUG)
 	e.HideBanner = true
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	proxyMappings := []*ProxyMapping{
-		&ProxyMapping{
-			RequestURI: "/webhooks/*",
-			TargetURL:  "http://localhost:9001",
-		},
+	proxyMappings, err := loadProxyMappings()
+	if err != nil {
+		e.Logger.Error(err)
 	}
 
 	if len(proxyMappings) > 0 {
@@ -37,8 +77,11 @@ func main() {
 				logging.ErrorAndExit(err.Error())
 			}
 			targets = append(targets, &middleware.ProxyTarget{URL: url})
+			e.Logger.Debug(fmt.Sprintf("Mapping URI group: %s to target endpoint: %s", proxyMapping.RequestURI, proxyMapping.TargetURL))
 			e.Group(proxyMapping.RequestURI, middleware.Proxy(middleware.NewRandomBalancer(targets)))
 		}
+
+		e.Logger.Info("Started tinylb...")
 
 		e.Logger.Fatal(e.StartAutoTLS(":443"))
 		//e.Start(":80")
