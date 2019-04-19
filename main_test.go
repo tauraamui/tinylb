5c22806d1fea3ed56803d145186ae1ec2ee6871e
tauraamui <adamplewis@pm.me>
2019-04-19 19:27:17 +0100

    convert single test functions into table testing structure

--- main_test.go
+++ main_test.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"io"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestLoadProxyMappingsSuccess(t *testing.T) {
-	t.Run("Test load proxy mappings success", func(t *testing.T) {
-		proxyMappings, err := loadProxyMappings(strings.NewReader("proxy /webhooks/* http://localhost:9001"))
-		assert.NoError(t, err)
+var proxyMappingTests = []struct {
+	name              string
+	reader            io.Reader
+	expectedError     error
+	expectedInterface *ProxyMapping
+}{
+	{
+		name:              "Test load proxy mapping success",
+		reader:            strings.NewReader("proxy /webhooks/* http://localhost:9001"),
+		expectedError:     nil,
+		expectedInterface: &ProxyMapping{RequestURI: "/webhooks/*", TargetURL: "http://localhost:9001"},
+	},
+}
 
-		assert.NotEmpty(t, proxyMappings)
-
-		assert.Equal(t, &ProxyMapping{RequestURI: "/webhooks/*", TargetURL: "http://localhost:9001"}, proxyMappings[0])
-	})
+func TestLoadProxyMapping(t *testing.T) {
+	for _, tt := range proxyMappingTests {
+		t.Run(tt.name, func(t *testing.T) {
+			proxyMappings, err := loadProxyMappings(tt.reader)
+			assert.Equal(t, tt.expectedError, err)
+			if tt.expectedInterface != nil {
+				assert.NotEmpty(t, proxyMappings)
+				assert.Equal(t, tt.expectedInterface, proxyMappings[0])
+			}
+		})
+	}
 }
